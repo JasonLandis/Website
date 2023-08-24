7969e91198451c24d09da7e8c813da3fdf47e534
Jason Landis <jasonelandis@gmail.com>
2023-08-24 03:51:41 -0400

    finished home animation

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
-function changeSize() {
-    document.getElementsByTagName("body").value = ;
+function ripple() {    
+    if (document.getElementById("ripple1").style.padding != "100%") {
+        document.getElementById("ripple1").style.transition = "all 1.5s linear";
+        document.getElementById("ripple1").style.padding = "100%";
+        document.getElementById("ripple2").style.transition = "none";
+        document.getElementById("ripple2").style.padding = "1%";
+        return;
+    }   
+
+    if (document.getElementById("ripple2").style.padding != "100%") {
+        document.getElementById("ripple2").style.transition = "all 1.5s linear";
+        document.getElementById("ripple2").style.padding = "100%";
+        document.getElementById("ripple3").style.transition = "none";
+        document.getElementById("ripple3").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple3").style.padding != "100%") {
+        document.getElementById("ripple3").style.transition = "all 1.5s linear";
+        document.getElementById("ripple3").style.padding = "100%";
+        document.getElementById("ripple4").style.transition = "none";
+        document.getElementById("ripple4").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple4").style.padding != "100%") {
+        document.getElementById("ripple4").style.transition = "all 1.5s linear";
+        document.getElementById("ripple4").style.padding = "100%";
+        document.getElementById("ripple5").style.transition = "none";
+        document.getElementById("ripple5").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple5").style.padding != "100%") {
+        document.getElementById("ripple5").style.transition = "all 1.5s linear";
+        document.getElementById("ripple5").style.padding = "100%";
+        document.getElementById("ripple6").style.transition = "none";
+        document.getElementById("ripple6").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple6").style.padding != "100%") {
+        document.getElementById("ripple6").style.transition = "all 1.5s linear";
+        document.getElementById("ripple6").style.padding = "100%";
+        document.getElementById("ripple7").style.transition = "none";
+        document.getElementById("ripple7").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple7").style.padding != "100%") {
+        document.getElementById("ripple7").style.transition = "all 1.5s linear";
+        document.getElementById("ripple7").style.padding = "100%";
+        document.getElementById("ripple8").style.transition = "none";
+        document.getElementById("ripple8").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple8").style.padding != "100%") {
+        document.getElementById("ripple8").style.transition = "all 1.5s linear";
+        document.getElementById("ripple8").style.padding = "100%";
+        document.getElementById("ripple9").style.transition = "none";
+        document.getElementById("ripple9").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple9").style.padding != "100%") {
+        document.getElementById("ripple9").style.transition = "all 1.5s linear";
+        document.getElementById("ripple9").style.padding = "100%";
+        document.getElementById("ripple10").style.transition = "none";
+        document.getElementById("ripple10").style.padding = "1%";
+        return;
+    }
+
+    if (document.getElementById("ripple10").style.padding != "100%") {
+        document.getElementById("ripple10").style.transition = "all 1.5s linear";
+        document.getElementById("ripple10").style.padding = "100%";
+        document.getElementById("ripple1").style.transition = "none";
+        document.getElementById("ripple1").style.padding = "1%";
+        return;
+    }    
 }

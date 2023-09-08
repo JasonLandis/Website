263f126785af8b7f80be81fb43534e8532894a06
Jason Landis <jasonelandis@gmail.com>
2023-09-08 00:22:04 -0400

    almost done

--- script.js
+++ script.js
@@ -16,83 +16,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-const start = document.getElementById("start");
-const games = document.getElementById("games");
-const puzzles = document.getElementById("puzzles");
-const about = document.getElementById("about");
+function scrolls(id) {
+    const targetSection = document.getElementById(id);
 
-let x = 0;
-function colorSwap() {
-    if (x == 0) {
-        start.style = "background: radial-gradient(#552900, #000000);";
-        games.style = "background: radial-gradient(#552900, #000000);";
-        puzzles.style = "background: radial-gradient(#552900, #000000);";
-        about.style = "background: radial-gradient(#552900, #000000);";
-        x = 1;
-    }
-
-    else if (x == 1) {
-        start.style = "background: radial-gradient(#4b4600, #000000);";
-        games.style = "background: radial-gradient(#4b4600, #000000);";
-        puzzles.style = "background: radial-gradient(#4b4600, #000000);";
-        about.style = "background: radial-gradient(#4b4600, #000000);";
-        x = 2;
-    }
-
-    else if (x == 2) {
-        start.style = "background: radial-gradient(#023100, #000000);";
-        games.style = "background: radial-gradient(#023100, #000000);";
-        puzzles.style = "background: radial-gradient(#023100, #000000);";
-        about.style = "background: radial-gradient(#023100, #000000);";
-        x = 3;
-    }
-
-    else if (x == 3) {
-        start.style = "background: radial-gradient(#004b3e, #000000);";
-        games.style = "background: radial-gradient(#004b3e, #000000);";
-        puzzles.style = "background: radial-gradient(#004b3e, #000000);";
-        about.style = "background: radial-gradient(#004b3e, #000000);";
-        x = 4;
-    }
-
-    else if (x == 4) {
-        start.style = "background: radial-gradient(#00334b, #000000);";
-        games.style = "background: radial-gradient(#00334b, #000000);";
-        puzzles.style = "background: radial-gradient(#00334b, #000000);";
-        about.style = "background: radial-gradient(#00334b, #000000);";
-        x = 5;
-    }
-
-    else if (x == 5) {
-        start.style = "background: radial-gradient(#00054b, #000000);";
-        games.style = "background: radial-gradient(#00054b, #000000);";
-        puzzles.style = "background: radial-gradient(#00054b, #000000);";
-        about.style = "background: radial-gradient(#00054b, #000000);";
-        x = 6;
-    }
-
-    else if (x == 6) {
-        start.style = "background: radial-gradient(#27004b, #000000);";
-        games.style = "background: radial-gradient(#27004b, #000000);";
-        puzzles.style = "background: radial-gradient(#27004b, #000000);";
-        about.style = "background: radial-gradient(#27004b, #000000);";
-        x = 7;
-    }
-
-    else if (x == 7) {
-        start.style = "background: radial-gradient(#4b0047, #000000);";
-        games.style = "background: radial-gradient(#4b0047, #000000);";
-        puzzles.style = "background: radial-gradient(#4b0047, #000000);";
-        about.style = "background: radial-gradient(#4b0047, #000000);";
-        x = 8;
-    }
-
-    else if (x == 8) {
-        start.style = "background: radial-gradient(#530000, #000000);";
-        games.style = "background: radial-gradient(#530000, #000000);";
-        puzzles.style = "background: radial-gradient(#530000, #000000);";
-        about.style = "background: radial-gradient(#530000, #000000);";
-        x = 0;
+    if (targetSection) {
+        targetSection.scrollIntoView({ behavior: "smooth" });
     }
 }
-

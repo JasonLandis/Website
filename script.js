78aeae0786807411047db454c4ed7e5948728462
Jason Landis <jasonelandis@gmail.com>
2023-09-10 00:37:57 -0400

    About page done

--- script.js
+++ script.js
@@ -1,25 +1,43 @@
-// Smooth scrolling to sections
-document.addEventListener("DOMContentLoaded", function() {
-    const navbarLinks = document.querySelectorAll("a");
-
-    navbarLinks.forEach(function(link) {
-        link.addEventListener("click", function(event) {
-            event.preventDefault();
-
-            const targetId = this.getAttribute("href").substring(1); // Get the target section ID
-            const targetSection = document.getElementById(targetId);
-
-            if (targetSection) {
-                targetSection.scrollIntoView({ behavior: "smooth" });
-            }
-        });
-    });
-});
-
 function scrolls(id) {
     const targetSection = document.getElementById(id);
 
     if (targetSection) {
         targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    if (id == 'start') {
+        document.getElementById('start-nav').style = "transform: scale(1.2); color: rgb(255, 135, 135)";
+        document.getElementById('constellation-nav').style = "none";
+        document.getElementById('circuit-nav').style = "none";
+        document.getElementById('about-nav').style = "none";
+
+        return;
+    }
+
+    if (id == 'constellation' || id == 'pathfinder') {
+        document.getElementById('start-nav').style = "none";
+        document.getElementById('constellation-nav').style = "transform: scale(1.2); color: rgb(127, 255, 115)";
+        document.getElementById('circuit-nav').style = "none";
+        document.getElementById('about-nav').style = "none";
+
+        return;
+    }
+
+    if (id == 'circuit' || id == 'tic' || id == 'words' || id == 'void' || id == 'dots' || id == 'halfway') {
+        document.getElementById('start-nav').style = "none";
+        document.getElementById('constellation-nav').style = "none";
+        document.getElementById('circuit-nav').style = "transform: scale(1.2); color: rgb(150, 143, 255)";
+        document.getElementById('about-nav').style = "none";
+
+        return;
+    }
+
+    if (id == 'about') {
+        document.getElementById('start-nav').style = "none";
+        document.getElementById('constellation-nav').style = "none";
+        document.getElementById('circuit-nav').style = "none";
+        document.getElementById('about-nav').style = "transform: scale(1.2); color: rgb(255, 229, 115)";
+
+        return;
     }
 }
